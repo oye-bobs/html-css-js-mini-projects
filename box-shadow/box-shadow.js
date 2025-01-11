8684b08cd5c37d69268192c12fe6117ffa20d9b6
utk09-NCL <utkarsh.t0298@gmail.com>
2025-01-11 21:52:40 +0000

    feat: Box Shadow JS started

---
+++ box-shadow/box-shadow.js
@@ -0,0 +1,19 @@
+const horizontal = document.getElementById("horizontal");
+const vertical = document.getElementById("vertical");
+const blur = document.getElementById("blur");
+const spread = document.getElementById("spread");
+const color = document.getElementById("color");
+const opacity = document.getElementById("opacity");
+const inset = document.getElementById("inset");
+const box = document.getElementById("box");
+const code = document.getElementById("code");
+const copyButton = document.getElementById("copy-button");
+
+const horizontalValue = document.getElementById("horizontal-value");
+const verticalValue = document.getElementById("vertical-value");
+const blurValue = document.getElementById("blur-value");
+const spreadValue = document.getElementById("spread-value");
+const colorValue = document.getElementById("color-value");
+const opacityValue = document.getElementById("opacity-value");
+
+
